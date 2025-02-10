bea182e0b3f1ee3a2377f8cae2c172bcd78c0e19
Ash D <ashley.d@montu.com.au>
2025-02-10 14:23:18 +1100

    fix: cancel booking api not using cancelledby

--- apps/api/v1/pages/api/bookings/[id]/_delete.ts
+++ apps/api/v1/pages/api/bookings/[id]/_delete.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest } from "next";
 
 import handleCancelBooking from "@calcom/features/bookings/lib/handleCancelBooking";
 import { defaultResponder } from "@calcom/lib/server";
-import { bookingCancelSchema } from "@calcom/prisma/zod-utils";
 
 import { schemaQueryIdParseInt } from "~/lib/validations/shared/queryIdTransformParseInt";
 
@@ -64,14 +63,20 @@ import { schemaQueryIdParseInt } from "~/lib/validations/shared/queryIdTransform
  *        description: User not found
  */
 async function handler(req: NextApiRequest) {
-  const { id, allRemainingBookings, cancellationReason } = schemaQueryIdParseInt
-    .merge(bookingCancelSchema.pick({ allRemainingBookings: true, cancellationReason: true }))
-    .parse({
-      ...req.query,
-      allRemainingBookings: req.query.allRemainingBookings === "true",
-    });
+  /**
+   *
+   * Note: Fix this parser, it is stripping out cancelledBy.
+   * Calcom didn't test the handler to see if the value was acutally passing in.
+   */
+  const { id, allRemainingBookings, cancellationReason, cancelledBy } = schemaQueryIdParseInt.merge(
+    schemaBookingCancelParams.pick({
+      allRemainingBookings: true,
+      cancellationReason: true,
+      cancelledBy: true,
+    })
+  );
   // Normalizing for universal handler
-  req.body = { id, allRemainingBookings, cancellationReason };
+  req.body = { id, allRemainingBookings, cancellationReason, cancelledBy };
   return await handleCancelBooking(req);
 }
 
