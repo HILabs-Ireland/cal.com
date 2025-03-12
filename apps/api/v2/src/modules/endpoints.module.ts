23abfb164e60530d2cde58e528cb8b453b6a0099
Frankie Lyons <frankie.lyons@hilabs.ie>
2025-03-12 12:22:57 +0000

    Remove usage from booking service and modules

--- apps/api/v2/src/modules/endpoints.module.ts
+++ apps/api/v2/src/modules/endpoints.module.ts
@@ -1,12 +1,33 @@
 import { PlatformEndpointsModule } from "@/ee/platform-endpoints-module";
+<<<<<<< HEAD
 import { BillingModule } from "@/modules/billing/billing.module";
+=======
+import { AtomsModule } from "@/modules/atoms/atoms.module";
+import { ConferencingModule } from "@/modules/conferencing/conferencing.module";
+import { DestinationCalendarsModule } from "@/modules/destination-calendars/destination-calendars.module";
+>>>>>>> 6530483014 (Remove usage from booking service and modules)
 import { OAuthClientModule } from "@/modules/oauth-clients/oauth-client.module";
 import { TimezoneModule } from "@/modules/timezones/timezones.module";
 import type { MiddlewareConsumer, NestModule } from "@nestjs/common";
 import { Module } from "@nestjs/common";
 
 @Module({
+<<<<<<< HEAD
   imports: [OAuthClientModule, BillingModule, PlatformEndpointsModule, TimezoneModule],
+=======
+  imports: [
+    OAuthClientModule,
+    PlatformEndpointsModule,
+    TimezoneModule,
+    UsersModule,
+    WebhooksModule,
+    DestinationCalendarsModule,
+    AtomsModule,
+    StripeModule,
+    ConferencingModule,
+    RouterModule,
+  ],
+>>>>>>> 6530483014 (Remove usage from booking service and modules)
 })
 export class EndpointsModule implements NestModule {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
