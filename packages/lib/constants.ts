5cc6680c96379b16d2bdbfb868da988a7a7b3dab
Frankie Lyons <frankie.lyons@hilabs.ie>
2025-03-18 13:01:39 +0000

    remove billing files

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -93,11 +93,6 @@ export const SEO_IMG_DEFAULT = `${CAL_URL}/og-image.png`;
 export const SEO_IMG_OGIMG = `${CAL_URL}/_next/image?w=1200&q=100&url=`;
 export const SEO_IMG_OGIMG_VIDEO = `${CAL_URL}/video-og-image.png`;
 export const IS_STRIPE_ENABLED = !!process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY;
-/** This has correct value only server side. When you want to use client side, go for IS_TEAM_BILLING_ENABLED_CLIENT. I think we should use the _CLIENT one only everywhere so that it works reliably everywhere on client as well as server  */
-export const IS_TEAM_BILLING_ENABLED = IS_STRIPE_ENABLED && HOSTED_CAL_FEATURES;
-
-export const IS_TEAM_BILLING_ENABLED_CLIENT =
-  !!process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY && HOSTED_CAL_FEATURES;
 
 export const FULL_NAME_LENGTH_MAX_LIMIT = 50;
 export const MINUTES_TO_BOOK = process.env.NEXT_PUBLIC_MINUTES_TO_BOOK || "5";
