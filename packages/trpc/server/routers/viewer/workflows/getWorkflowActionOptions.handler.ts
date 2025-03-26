5ebf38fe549cbea33dfd6d83cd35e2f08a351df5
Frankie Lyons <frankie.lyons@hilabs.ie>
2025-03-26 17:06:20 +0000

    getoptions call fix check

--- packages/trpc/server/routers/viewer/workflows/getWorkflowActionOptions.handler.ts
+++ packages/trpc/server/routers/viewer/workflows/getWorkflowActionOptions.handler.ts
@@ -1,5 +1,4 @@
 import { getWorkflowActionOptions } from "@calcom/features/ee/workflows/lib/getOptions";
-import { IS_SELF_HOSTED } from "@calcom/lib/constants";
 import { getTranslation } from "@calcom/lib/server/i18n";
 import type { TrpcSessionUser } from "@calcom/trpc/server/trpc";
 
@@ -14,8 +13,6 @@ type GetWorkflowActionOptionsOptions = {
 export const getWorkflowActionOptionsHandler = async ({ ctx }: GetWorkflowActionOptionsOptions) => {
   const { user } = ctx;
 
-  const hasOrgsPlan = !!user.profile?.organizationId;
-
   const t = await getTranslation(ctx.user.locale, "common");
-  return getWorkflowActionOptions(t, IS_SELF_HOSTED, IS_SELF_HOSTED || hasOrgsPlan);
+  return getWorkflowActionOptions(t);
 };
